f8e53156a6273825abcfa489b23da9c8a854574e
Benjamin Weisman <benjamin.weisman15@ncf.edu>
2020-11-28 20:34:44 -0500

    NetworkX implementation

--- assignment10.py
+++ assignment10.py
@@ -1,55 +1,18 @@
 import pandas as pd
-
-#Importing and Initializing our datasets
-routes = pd.read_csv('routes.dat.txt', names=['airline', 
-                                              'airline_id',
-                                              'source_airport',
-                                              'source_airport_id',
-                                              'destination_airport',
-                                              'destination_airport_id',
-                                              'codeshare', 'stops',
-                                              'equipment'])
-
-planes = pd.read_csv('planes.dat.txt',)
-
-#Showing our datasets
-print(routes)
-print(planes)
+import networkx as nx
+import matplotlib.pyplot as mp
+from datetime import datetime
+import numpy as np
+from array import array
+from scipy.stats.mstats import gmean
 
 
+df = pd.read_csv ('Code Cap.csv')
 
-# To filter only NY airports
-lga = routes["source_airport"] == "LGA"
-lga = routes[lga]
-len(lga)
+print(df)
 
-jfk = routes["source_airport"] == "JFK"
-jfk = routes[jfk]
-len(jfk)
+G=nx.from_pandas_edgelist(df, source='SOURCE', target='TARGET', edge_attr='CAPACITY', create_using=nx.DiGraph())
 
 
-# There are 7 nonstop flights from JFK to SFO
-jfk_to_sfo = (routes["source_airport"] == "JFK") & (routes["destination_airport"] == "SFO")
-jfk_to_sfo = routes[jfk_to_sfo]
-len(jfk_to_sfo)
-
-
-
-
-
-
-sfo = routes["destination_airport"] == "SFO"
-sfo = routes[sfo]
-len(sfo)
-
-# list of airports with flights to SFO
-source_to_sfo = list(sfo["source_airport"])
-len(source_to_sfo)
-
-
-
-# Select airports that are midpoint for flights from LGA to SFO
-for a in source_to_sfo:
-    for b in lga["destination_airport"]:
-        if a == b:
-            print("LGA", a, "SFO")
+nx.draw_networkx(G)
+mp.show()
