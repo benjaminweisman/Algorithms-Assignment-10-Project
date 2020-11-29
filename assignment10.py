092f8464291d45be03aa24595c94268895405173
palazzojulian <50282600+palazzojulian@users.noreply.github.com>
2020-11-28 21:38:31 -0500

    Update assignment10.py

--- assignment10.py
+++ assignment10.py
@@ -11,8 +11,30 @@ df = pd.read_csv ('Code Cap.csv')
 
 print(df)
 
-G=nx.from_pandas_edgelist(df, source='SOURCE', target='TARGET', edge_attr='CAPACITY', create_using=nx.DiGraph())
+G=nx.from_pandas_edgelist(df, source='SOURCE', target='TARGET', edge_attr='CAPACITY', create_using=nx.MultiDiGraph())
 
 
 nx.draw_networkx(G)
 mp.show()
+nx.edges(G)
+len(list(nx.edges(G)))
+
+
+for u in G:
+    for v in G:
+        if nx.has_path(G,u,v):
+            print(G.get_edge_data(u,v))
+            
+            
+G.get_edge_data('EWR', 'SFO')
+
+
+
+
+# M=nx.DiGraph()
+# M.add_edge(1,2,weight=2)
+# nx.edges(M)
+# # nx.floyd_warshall(G,1,2)
+# M.get_edge_data(1,2)
+
+
